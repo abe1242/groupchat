38989e717f5ca416625a5d3055905643dc067f1e
Abhishek BM <abmdn1242@gmail.com>
2023-04-28 22:18:25 +0530

    perfected the database script

--- sql/groupchat.sql
+++ sql/groupchat.sql
@@ -1,20 +1,22 @@
--- Database: `groupchat`
+DROP DATABASE IF EXISTS `groupchat`;
+CREATE DATABASE `groupchat`;
+USE `groupchat`;
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL UNIQUE,
   `passwd` varchar(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS `groups` (
+CREATE TABLE `groups` (
   `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `user_id` int NOT NULL,
   FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS `messages` (
+CREATE TABLE `messages` (
   `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `message` text NOT NULL,
   `post_date` datetime NOT NULL,
